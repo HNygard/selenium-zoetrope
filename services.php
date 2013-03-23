267d406407c6a351b965429c5d42b97ac22dee11
Hallvard Nygard <hallvard.nygard@gmail.com>
2013-03-23 15:48:46 +0100

    Formatting

--- services.php
+++ services.php
@@ -6,7 +6,7 @@
 abstract class BackgroundService {
 	protected $pid_file_name;
 	protected $output_file_name;
-	
+
 	protected function startProcess($command) {
 		global $selenium_host, $running_ssh;
 		// Store the output of the background service and it's pid in temporary
@@ -17,7 +17,7 @@ abstract class BackgroundService {
 		//$this->pid_file_name =  $unique_file . '.pid';
 		//$this->output_file_name = $unique_file . '.out';
 		//unlink($unique_file);
-		
+
 		$unique = time().rand(0,100);
 		$this->pid_file_name = 'background-service-'.$unique.'.pid';
 		$this->output_file_name = 'background-service-'.$unique.'.out';
@@ -34,27 +34,27 @@ abstract class BackgroundService {
 		else {
 			exec($command);
 		}
-		
+
 		$remaining_tries = 60;
 		while ($remaining_tries > 0 && !$this->isReady()) {
 			sleep(2);
 			--$remaining_tries;
 		}
-		
+
 		if ($remaining_tries == 0) {
 			echo 'Background service failed:' . PHP_EOL;
 			echo $this->getOutput();
 			throw new Exception('Background service did not start.');
 		}
 	}
-	
+
 	protected function isReady() {
 		return TRUE;
 	}
-	
+
 	public function getOutput() {
 		global $selenium_host, $running_ssh;
-		
+
 		if($running_ssh) {
 			exec('ssh '.$selenium_host.' \'cat '.$this->output_file_name.'\'', $output);
 			return implode($output, PHP_EOL);
@@ -63,7 +63,7 @@ abstract class BackgroundService {
 			return file_get_contents($this->output_file_name);
 		}
 	}
-	
+
 	public function getPid() {
 		global $selenium_host, $running_ssh;
 		if(isset($this->pid_file_name)) {
@@ -79,7 +79,7 @@ abstract class BackgroundService {
 			return NULL;
 		}
 	}
-	
+
 	public function __destruct() {
 		global $selenium_host, $running_ssh;
 		$pid = $this->getPid();
@@ -104,9 +104,9 @@ abstract class BackgroundService {
  */
 interface XWindowsServiceInterface {
 	public function getDisplay();
-	
+
 	public function getWidth();
-	
+
 	public function getHeight();
 }
 
@@ -117,24 +117,24 @@ class XvfbBackgroundService extends BackgroundService implements XWindowsService
 	protected $displayNumber;
 	protected $width;
 	protected $height;
-	
+
 	public function __construct($display_number, $width = 1600, $height = 1200) {
 		$this->displayNumber = $display_number;
 		$this->width         = $width;
 		$this->height        = $height;
-		
+
 		$command = '/usr/bin/Xvfb :' . $this->displayNumber . ' -ac -screen 0 ' . $this->width . 'x' . $this->height . 'x24';
 		$this->startProcess($command);
 	}
-	
+
 	public function getDisplay() {
 		return ':' . $this->displayNumber;
 	}
-	
+
 	public function getWidth() {
 		return $this->width;
 	}
-	
+
 	public function getHeight() {
 		return $this->height;
 	}
@@ -142,7 +142,7 @@ class XvfbBackgroundService extends BackgroundService implements XWindowsService
 
 interface SeleniumServiceInterface {
 	public function getHost();
-	
+
 	public function getPort();
 }
 
@@ -151,7 +151,7 @@ interface SeleniumServiceInterface {
  */
 class SeleniumBackgroundService extends BackgroundService implements SeleniumServiceInterface {
 	protected $port;
-	
+
 	public function __construct(XWindowsServiceInterface $display, $port) {
 		$this->port = $port;
 
@@ -159,16 +159,16 @@ class SeleniumBackgroundService extends BackgroundService implements SeleniumSer
 
 		$this->startProcess($command);
 	}
-	
+
 	public function getHost() {
 		global $selenium_host;
 		return $selenium_host;
 	}
-	
+
 	public function getPort() {
 		return $this->port;
 	}
-	
+
 	protected function isReady() {
 		return selenium_is_running($this->getHost(), $this->getPort());
 	}
@@ -177,16 +177,16 @@ class SeleniumBackgroundService extends BackgroundService implements SeleniumSer
 class SeleniumExternalService implements SeleniumServiceInterface {
 	protected $host;
 	protected $port;
-	
+
 	public function __construct($host, $port) {
 		$this->host = $host;
 		$this->port = $port;
 	}
-	
+
 	public function getHost() {
 		return $this->host;
 	}
-	
+
 	public function getPort() {
 		return $this->port;
 	}
@@ -219,13 +219,13 @@ class SeleniumTest {
 	protected $bootstrapFile;
 	protected $testClassName;
 	protected $testFileName;
-	
+
 	public function __construct(SeleniumServiceInterface $selenium_server, $test_file, $base_url, $browser = '*firefox') {
         global $results_directory;
 
 		$this->testFileName = $test_file;
 		$this->testClassName = pathinfo($test_file, PATHINFO_FILENAME);
-		
+
 		// Construct a bootstrap file for each test, that contains global variables
 		// for the Selenium server to connect to, as well as the browser and URL to
 		// run against. The alternative to this would be to pass command line
@@ -243,18 +243,18 @@ class SeleniumTest {
 
         // TODO: remove debug
         echo PHP_EOL.PHP_EOL.$text.PHP_EOL.PHP_EOL;
-		
+
 		// Store a unique bootstrap file per test.
 		$directory = sys_get_temp_dir() . '/selenium-bootstrap';
 		@mkdir($directory);
 		$this->bootstrapFile = tempnam($directory, 'bootstrap-');
-		
+
 		// echo 'Storing bootstrap: ' . $this->bootstrapFile . PHP_EOL;
 		// echo 'Contents: ' . $text . PHP_EOL;
-		
+
 		file_put_contents($this->bootstrapFile, $text);
 	}
-	
+
 	public function run($results_file) {
 		$old_working_directory = getcwd();
 		chdir(dirname($this->testFileName));
@@ -264,11 +264,11 @@ class SeleniumTest {
 		echo $output;
 		chdir($old_working_directory);
 	}
-	
+
 	public function getTestClassName() {
 		return $this->testClassName;
 	}
-	
+
 	public function __destruct() {
 		unlink($this->bootstrapFile);
 	}
